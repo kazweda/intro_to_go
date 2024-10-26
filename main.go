137f22be6babff763e9ab1bab8656414c21db339
Kazuaki Ueda <kazuaki.ueda@gmail.com>
2024-10-26 15:57:11 +0900

    fix #3 named results

--- main.go
+++ main.go
@@ -6,6 +6,13 @@ func add(x int, y int) int {
 	return x + y
 }
 
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
+}
+
 func main() {
 	fmt.Println(add(42, 13))
+	fmt.Println(split(17))
 }
